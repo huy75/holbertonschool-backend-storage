ba8a22c161f6ab763e22d46928763b9fd091276e
Huy Nguyen <huy.nguyen@holbertonschool.com>
2021-07-29 14:23:20 +0000

    feat tasks 4 & 5

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,5 +1,7 @@
 -- Creates a trigger that decreases the quantity
 -- of an item after adding a new order
+-- New & OLD are MySQL extensions to triggers
+-- enable to access columns in the rows affected by a trigger
 
 CREATE TRIGGER decrease_quantity
 AFTER INSERT ON orders
