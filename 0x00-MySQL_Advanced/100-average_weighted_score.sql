b6c182d003cf7990f9950a61f0c17dff94a95f08
Huy Nguyen <huy.nguyen@holbertonschool.com>
2021-07-30 11:19:57 +0000

    fix add join condition

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -9,6 +9,7 @@ BEGIN
     SET average_score = (SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
         FROM corrections
         INNER JOIN projects
+        ON projects.id = corrections.project_id
         WHERE corrections.user_id = user_id)
     WHERE users.id = user_id;
 END $$
