5a26e13fbbed66f81e14d0f6474406d3640e9bd7
Huy Nguyen <huy.nguyen@holbertonschool.com>
2021-08-04 15:16:31 +0000

    feat task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,8 @@
 """ Module for Redis db """
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable, Optional
+
 
 class Cache:
     """ Class for methods that operate a caching system """
@@ -12,8 +13,9 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        """ 
+    def store(self,
+              data: Union[str, bytes, int, float]) -> str:
+        """
         Method takes a data argument and returns a string
         Generate a random key (e.g. using uuid), store the input data in Redis
         using the random key and return the key
@@ -21,3 +23,21 @@ class Cache:
         key = str(uuid4())
         self._redis.mset({key: data})
         return key
+
+    def get(self,
+            key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """
+        Retrieves data stored at a key
+        converts the data back to the desired format
+        """
+        data = self._redis.get(key)
+        return fn(data) if fn else data
+
+    def get_str(self, data: str) -> str:
+        """ get a string """
+        return self._redis.get(data).decode('utf-8')
+
+    def get_int(self, data: str) -> int:
+        """ get an int """
+        return int(self._redis.get(data))
